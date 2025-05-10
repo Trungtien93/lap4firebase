3731733facb4d2f79693639d84df90d0ce04008d
Trungtien93 <kasadin2k@gmail.com>
2025-05-11 00:03:02 +0700

    final

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,5 @@ const defaultConfig = getDefaultConfig(__dirname);
 
 defaultConfig.resolver.sourceExts.push('cjs'); // Thêm hỗ trợ .cjs
 defaultConfig.resolver.unstable_enablePackageExports = false; // 👈 Dòng fix lỗi auth crash Hermes
-<<<<<<< HEAD
-=======
 
->>>>>>> e193fd014d97d193c2fc82c6f98513d705ce651c
 module.exports = defaultConfig;
